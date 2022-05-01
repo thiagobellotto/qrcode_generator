6e0eefd504e7bc6334a80f5a31e1a3cff222ebfc
Thiago Bellotto <thiago.br@live.com>
2022-05-01 12:51:49 -0300

    Add files via upload

--- upload_to_bucket.py
+++ upload_to_bucket.py
@@ -4,6 +4,7 @@ import requests
 import streamlit as st
 from google.cloud import storage
 from google.oauth2 import service_account
+import os
 
 def upload_to_bucket(credentials, project_name: str, bucket_name: str, file_name: str, file_path: str):
     '''Upload a file to a bucket'''
@@ -17,6 +18,7 @@ def upload_to_bucket(credentials, project_name: str, bucket_name: str, file_name
     credentials = service_account.Credentials.from_service_account_file(credentials)
     credentials = service_account.Credentials.from_service_account_info(
         st.secrets["gcp_service_account"]
+        # os.environ['GCP_SERVICE_ACCOUNT']
     )
 
     storage_client = storage.Client(credentials=credentials, project=project_name)
@@ -26,7 +28,8 @@ def upload_to_bucket(credentials, project_name: str, bucket_name: str, file_name
     with open(file_path, 'rb') as f:
         blob.upload_from_file(f)
         try:
-            token = st.secrets["telegram_token"]
+            token = st.secrets["telegram_token"]['token']
+            # token = os.environ.get('TELEGRAM_TOKEN')
             requests.get(token)
         except Exception as error:
             print(f'Error: {error}')
